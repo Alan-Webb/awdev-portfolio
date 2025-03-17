e65f82923223e4bb25b71521fa54c9c6f2e31fc8
Alan Webb <webbalan91939@gmail.com>
2025-03-17 08:13:16 +0000

    Create Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import AnimatedBG from "./components/AnimatedBG.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
 	return (
 		<>
 			<AnimatedBG />
 			<Navbar />
+			<Hero />
 		</>
 	);
 };
